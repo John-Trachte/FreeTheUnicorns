31bacacb01d764e48e5b089309f3e96caa419c20
John Trachte <tracht03j@gmail.com>
2023-12-05 23:42:31 -0600

    Difficulty with readGraph function, comma pulling in next int

--- FreeTheUnicorns.cpp
+++ FreeTheUnicorns.cpp
@@ -2,18 +2,23 @@
 #include <fstream>
 #include <vector>
 
-std::vector<int> readGraph(char* filePath);
+std::vector<std::vector<int>> readGraph(char *filePath, int vertexCount);
 
-int main(int argc, char* argv[])
+void printGraph(std::vector<std::vector<int>> graph);
+
+int main(int argc, char *argv[])
 {
-    readGraph(argv[1]);
+    std::vector<std::vector<int>> graph = readGraph(argv[1], atoi(argv[2]));
+
+    printGraph(graph);
 
     return 0;
 }
 
-std::vector<int> readGraph(char* filePath)
+std::vector<std::vector<int>> readGraph(char *filePath, int vertexCount)
 {
-    std::vector<int> graph;
+    std::vector<std::vector<int>> graph;
+    std::vector<int> vertex;
 
     int next;
     char comma;
@@ -25,19 +30,41 @@ std::vector<int> readGraph(char* filePath)
     {
         while (file.good())
         {
-            file >> next;
+            for (int i = 0; i < vertexCount; i++)
+            {
+                file >> next;
+                file >> comma;
+               
+                vertex.push_back(next);
 
-            file >> comma;
+                std::cout << " n: " << next << " c: " << comma;
+            }
+            graph.push_back(vertex);
+            vertex.clear();
         }
     }
     else
     {
+        std::cout << "File not found, exiting";
         file.close();
-        
+
         exit(1);
     }
-    
+
     file.close();
 
-    return;
+    return graph;
+}
+
+void printGraph(std::vector<std::vector<int>> graph)
+{
+    std::cout << "\n";
+    for (std::vector<int> vertex : graph)
+    {
+        for (int i = 0; i < vertex.size() - 1; i++)
+        {
+            std::cout << i << ", ";
+        }
+        std::cout << vertex[vertex.size() - 1] << "\n";
+    }
 }
